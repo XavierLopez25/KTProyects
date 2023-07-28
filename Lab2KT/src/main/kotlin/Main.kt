c7990ab22488c74f22d4fbf452d8835b97593501
XavierLopez25 <lop22716@uvg.edu.gt>
2023-07-27 19:27:54 -0600

    Task 2 completed.

--- Lab2KT/src/main/kotlin/Main.kt
+++ Lab2KT/src/main/kotlin/Main.kt
@@ -2,13 +2,21 @@ fun main(args: Array<String>) {
 
     //Task 1: Calculate average of a list of numbers
     val numberList = listOf(1,2,3,4,5,6,7,8,9,10)
-    println("The average of the list is: ${numberAverage(numberList)}")
+    println("The average of the list is: ${numberAverage(numberList)} \n")
 
+    //Task 2: Extract only odd numbers from a list
+    val oddNumbers: List<Int> = numberList.filter { it % 2 == 1 }
+    println("The odd numbers from the list are: $oddNumbers \n")
+
+    
 
 }
 
 fun numberAverage(_numbersList: List<Int>): Double{
-    val sum_numbers = _numbersList.reduce{counter, num -> counter + num}
-    return sum_numbers.toDouble()/_numbersList.size
+    val sumNumbers = _numbersList.reduce{ counter, num -> counter + num}
+    return sumNumbers.toDouble()/_numbersList.size
 }
+
+
+
 
